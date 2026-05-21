742dbe1be18be3da5fe69e8cc3c1b9d41cd942a2
opinaka <mohammed.attik@opinaka.com>
2026-05-21 14:17:50 +0200

    cleanup: remove entrypoint.sh (Nginx only, not needed for Android)

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Substitution de la variable BACKEND_URL dans config.js
-BACKEND_URL=${BACKEND_URL:-http://localhost:8000}
-envsubst '${BACKEND_URL}' < /usr/share/nginx/html/config.template.js > /usr/share/nginx/html/config.js
-nginx -g 'daemon off;'
